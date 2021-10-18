76dc9c7b904a5e1ec66e3cd7d49a39013f422329
Yadomaru <momoami14@gmail.com>
2021-10-18 23:23:36 +0900

    定期実行するプログラムを追加

---
+++ src/scheduler.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, abort
+import os
+import bs
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+MY_LINE_USER_ID = os.environ["MY_LINE_USER_ID"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+
+def main():
+  m = bs.Message()
+  text = m.makeMessage()
+  line_bot_api.push_message(MY_LINE_USER_ID, messages=text)
+
+if __name__ == "__main__":
+    main()
